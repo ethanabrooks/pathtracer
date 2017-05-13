30c4338970c58eeb6e8a24202d0b2a39bd8f96c7
Ethan <ethanabrooks@gmail.com>
2017-05-13 13:17:04 -0400

    finished breaking out into separate files

--- app/Main.hs
+++ app/Main.hs
@@ -2,31 +2,36 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeOperators   #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main ( main
             , RGB8
             ) where
 
 import Control.Lens
-import Data.List
 import Codec.Picture
 import Control.Monad
 import Control.Monad.ST
 import Data.Array.Repa (Array, DIM1, DIM2, U, D, Z (..), (:.)(..), (!))
 import System.Environment (getArgs)
 import System.FilePath (replaceExtension)
-import qualified Codec.Picture.Types as M
-import qualified Data.Array.Repa     as R -- for Repa
-import Lib (Vec3, Rays, _num)
+import qualified Codec.Picture.Types          as M
+import qualified Data.Array.Repa              as R -- for Repa
+import qualified Data.Array.Repa.Shape        as S
+import qualified Data.Array.Repa.Repr.Unboxed as U
+import qualified Data.Vector                  as V
 import Object
 import Data.Maybe
+import Triple (Triple (..), RGB8, Vec3, normalize)
+import Lib (flatten, Ray (..), Canvas, inf, mapIndex)
+import Data.Vector (Vector)
 
-type RGB8 = (Pixel8, Pixel8, Pixel8)
+type RGB8' = (Pixel8, Pixel8, Pixel8)
 
 
 
 -- | Get image with true color pixels from manifest Repa array.
-toImage :: Array U DIM2 RGB8 -> Image PixelRGB8
+toImage :: Array U DIM2 RGB8' -> Image PixelRGB8
 toImage a = generateImage gen width height
   where Z :. width :. height = R.extent a
         gen x y = let (r,g,b) = a ! (Z :. x :. y)
@@ -34,9 +39,6 @@ toImage a = generateImage gen width height
 
 
 -- | Paramters
-imgHeight = 1200
-imgWidth  = 1200
-cameraDepth = 10 
 
 numRays = imgHeight * imgWidth
 
@@ -48,11 +50,16 @@ imgDimensions = (Z :. imgHeight :. imgWidth)
 numIters :: Int
 numIters = 100
 
-objects = []
-
-type ImageArray = Array D DIM2 RGB8
+type ImageArray = Array D DIM2 RGB8'
 
 l = 100000
+
+canvas :: Canvas
+canvas = R.fromFunction (Z :. imgHeight :. imgWidth) $ const black
+
+blankCanvas :: Canvas
+blankCanvas = R.fromFunction (Z :. imgHeight :. imgWidth) $ const white
+
 
 main :: IO ()
 main = do
@@ -66,15 +73,48 @@ main = do
 repaImg :: ImageArray
 repaImg = R.fromFunction imgDimensions originalFnc
 
-newRGB :: Int -> Int -> Int -> RGB8
+newRGB :: Int -> Int -> Int -> RGB8'
 newRGB x y z = (pixel x, pixel y, pixel z)
   where pixel = fromIntegral . min 0xff
 
-originalFnc :: DIM2 -> RGB8
+originalFnc :: DIM2 -> RGB8'
 originalFnc (Z :. x :. y) =
   let (q, r) = x `quotRem` max 3 y
   in  newRGB q r (q + r + 30)
 
+
+--- ################################################
+
+-- | Paramters
+imgHeight = 2 :: Int --1200
+imgWidth  = 4 :: Int --1200
+cameraDepth = 10 :: Double
+
+black = pure 0 :: RGB8
+white = pure 1 :: RGB8
+some_vec = pure 1
+
+infPlane = Object 
+  { _color      = black
+  , _light      = False
+  , _reflective = True
+  , _form       = InfinitePlane
+             { _normal = some_vec
+             , _point  = some_vec }
+  }
+
+raysFromCam :: Array D DIM1 Ray
+raysFromCam = flatten $ mapIndex camToPixelRay canvas
+
+camToPixelRay :: DIM2 -> Ray
+camToPixelRay (Z :. i :. j)  = Ray { _origin   = pure 0
+                                   , _vector   = Triple i' j' cameraDepth }
+  where (i', j') = (fromIntegral i, fromIntegral j)
+
+
+
+objects :: Vector Object
+objects = V.fromList [infPlane] 
 
 -- repaImg' = vec3ToImage $ iterate rayTrace blankCanvas !! numIters :: ImageArray
 
@@ -83,24 +123,79 @@ originalFnc (Z :. x :. y) =
 --   where (_, canvas') = until allTerminal bounce (raysFromCam, canvas)
 --         allTerminal (nonTerminalRays, _)  = _num nonTerminalRays == 0
 
-bounce :: (Rays, Vec3) -> (Rays, Vec3)
-bounce = id -- TODO
+getNormal :: Form -> Vec3
+getNormal form = _normal form
+
+toSphericalCoords :: Vec3 -> (Double, Double)
+toSphericalCoords coord = (theta, phi)
+  where [theta, phi]  = map acos [x', z]
+        Triple x y z  = normalize coord
+        Triple x' _ _ = normalize $ Triple x y 0
+
+fromSphericalCoords :: Floating t => t -> t -> Triple t
+fromSphericalCoords theta phi = Triple x y z
+  where x = sin phi * cos theta
+        y = sin phi * sin theta
+        z = cos phi
+
+
+reflect :: Vec3 -> Vec3 -> Triple Double
+normal `reflect` vector = fromSphericalCoords theta phi
+  where angles         = map toSphericalCoords [-vector, normal]
+        [thetas, phis] = [map fst angles, map snd angles]
+        [theta, phi]   = [vectorAngle + 2 * (normalAngle - vectorAngle)
+                         | [vectorAngle, normalAngle] <- [thetas, phis]]
+
+
+closest :: Maybe Double -> Maybe Double -> Ordering
+closest d1 d2 = compare d1' d2'
+  where [d1', d2'] = map (fromMaybe inf) [d1, d2]
+
+
+getClosestObject :: Ray -> Maybe Object
+getClosestObject ray = fmap (const closestObject) closestDistance
+  where distances          = V.map (distanceFrom ray . _form) objects   :: Vector (Maybe Double)
+        compareSnd (_, d1) (_, d2) = closest d1 d2
+        (closestObject, closestDistance) = V.minimumBy compareSnd $ V.zip objects distances
   
-bounceRays :: Rays -> Rays
-bounceRays rays = rays
+addColor :: Maybe Object -> RGB8 -> RGB8
+addColor object pixel = maybe black ((pixel *) . _color) object
 
 
+bounce ray object distance = Just $ Ray { _origin = origin, _vector = vector }
+  where origin = march ray distance                             :: Vec3
+        vector = getNormal (_form object) `reflect` _vector ray :: Vec3
 
--- rayTrace (rays, canvas, lengths) = foldl
---     (\(rays, canvas, lengths) object ->
---       let distanceToObject = distanceBetween object rays
-          
---       in  ( object `reflect` rays
---           , colorWithTerminalRays, newDistances))
---     (raysFromCam, blankCanvas, infDistance) objects
 
--- reflect :: Elements -> Object -> Elements
--- reflect (rays, canvas, lengths) object = (newRays, newCanvas, distances)
---   where distanceToObject  = distanceBetween object rays
---         intersectionPoint = intersection object rays
---         closer            = 
+closestTo :: Ray -> (Maybe Double, Object)
+closestTo ray = V.minimumBy (\(d1, _) (d2, _) -> closest d1 d2) $ V.zip distances objects
+                                                             :: (Maybe Double, Object) 
+  where distances = V.map (distanceFrom ray . _form) objects :: Vector (Maybe Double)
+
+
+allTerminal ::
+  (R.Source r (Maybe a), Monad m, S.Shape sh) =>
+  Array r sh (Maybe a) -> m Bool
+allTerminal rays = R.foldAllP (&&) True isTerminal
+  where isTerminal = R.map isNothing rays
+
+
+update :: RGB8 -> Ray -> (RGB8, Maybe Ray)
+update pixel ray = (addColor object' pixel, bounce ray object =<< mDistance)
+  where (mDistance, object) = closestTo ray
+        object' = fmap (const object) mDistance
+
+rayTrace :: Array D DIM1 (RGB8, Maybe Ray)
+rayTrace = until allRaysTerminal updateAll $ R.zipWith (,) canvas' rays'
+                                           
+  where allRaysTerminal :: Array D DIM1 (RGB8, Maybe Ray) -> Bool
+        allRaysTerminal = fromJust . allTerminal . R.map snd
+
+        updateIfNonTerminal :: RGB8 -> Maybe Ray -> (RGB8, Maybe Ray)
+        updateIfNonTerminal = maybe (black, Nothing) . update
+
+        updateAll :: Array D DIM1 (RGB8, Maybe Ray) -> Array D DIM1 (RGB8, Maybe Ray)
+        updateAll = R.map . uncurry $ updateIfNonTerminal
+
+        canvas' = flatten blankCanvas    :: Array D DIM1 RGB8
+        rays'   = R.map Just raysFromCam :: Array D DIM1 (Maybe Ray)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,175 +1,24 @@
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE AllowAmbiguousTypes #-}
-{-# LANGUAGE TypeOperators #-}
-module Lib ( Rays (..)
-           , Vec1
-           , Vec3
+module Lib ( Ray (..)
+           , Canvas
            , inf
-           , lessThan
-           , filterWith
-           , emptyRays
            , reshape
            , expandDim
            , flatten
+           , mapIndex
            ) where
 
-import Control.Lens
 import qualified Data.Array.Repa     as R -- for Repa
-import Data.Array.Repa (Array, DIM1, DIM2, U, D, Z (..), (:.)(..), (!), (*^), All)
-import Data.Array.Repa hiding ((++), map, reshape)
-import Data.Array.Repa.Repr.Unboxed (Unbox)
-import Data.Array.Repa.Eval (Elt)
-import Codec.Picture
 import qualified Data.Array.Repa.Shape as S
+import Triple (Vec3, RGB8)
+import Data.Array.Repa (Array, DIM1, DIM2, D)
 
+type Canvas = Array D DIM2 RGB8
+data Ray = Ray { _origin   :: Vec3
+               , _vector   :: Vec3 }
 
-data FooBar
-  = Foo { _x :: [Int], _y :: Int }
-  | Bar { _x :: [Int] } deriving (Show)
-
-makeLenses ''FooBar
-
-foo = Foo { _x = [0..5], _y = 6 }
-justSix = foo ^? y
-x_ = foo ^. x
-foo' = foo & x %~ map (+ 1)
-foo'' = foo & x .~ [0..9]
-foo''' = foo & x . ix 2 .~ 8
-
-d1 :: Array D (Z :. Int) Double
-d1 = R.fromFunction (Z :. 10) $ \(Z :. x) -> case x of
-                                                  1 -> 1
-                                                  3 -> 3
-                                                  5 -> 5
-                                                  _ -> 1 / 0
-
--- | Paramters
-imgHeight = 2--1200
-imgWidth  = 4--1200
-cameraDepth = 10 
-
-numRays = imgHeight * imgWidth
-
-imgDimensions :: DIM2
-imgDimensions = (Z :. imgHeight :. imgWidth)
- 
-
-d2 :: Array D (Z :. Int) Double
-d2 = R.fromFunction (Z :. 10) $ \(Z :. x) -> fromIntegral x
- 
-
-d3 :: Array D (Z :. Int) Double
-d3 = R.fromFunction (Z :. 5) $ \(Z :. x) -> fromIntegral x
-
-
-dInf :: Array D (Z :. Int) Double
-dInf = R.fromFunction (Z :. round inf) $ const 0.0
-
-u1 :: Array U (Z :. Int) Double
-u1 = R.computeUnboxedS d1
-
-u2 :: Array U (Z :. Int) Double
-u2 = R.computeUnboxedS d2
-
-u3 :: Array U DIM2 Double
-u3 = R.fromListUnboxed (Z :. 2 :. 2) [1, 2, 3, 4]
-
-inf = 1.0 / 0.0 :: Double
-
-rangeD1 :: Array D (Z :. Int) Int
-rangeD1 = R.traverse d1 id (\_ (Z :. i) -> i)
-
-type ImageArray = Array D DIM2 RGB8
-type RGB8 = (Pixel8, Pixel8, Pixel8)
-type Vec3 = Array D DIM2 Double
-type Vec1 = Array D DIM1 Double
-data Rays = Rays { _origins   :: Vec3
-                 , _vectors   :: Vec3
-                 , _distances :: Array U DIM1 Double
-                 , _pixels    :: Array U DIM1 Int
-                 , _num       :: Int }
-
-makeLenses ''Rays
-
-instance Show (Rays) where
-  show Rays { _origins   = origins
-            , _vectors   = vectors
-            , _pixels    = pixel
-            , _distances = distance
-            , _num       = num } = "{ \n " ++ originsStr ++
-                                  ",\n "  ++ distanceStr ++ 
-                                  ",\n "  ++ show num ++ "\n}"
-       where originsStr  = show $ R.computeUnboxedS origins
-             distanceStr = show $ distance
-              
-
-newRGB :: Int -> Int -> Int -> RGB8
-newRGB x y z = (pixel x, pixel y, pixel z)
-  where pixel = fromIntegral . min 0xff
-
-lessThan :: R.Shape sh => Array D sh Double -> Array D sh Double -> Array D sh Bool
-lessThan a1 a2 = R.fromFunction (R.extent a1) (\coord -> (a1 ! coord) Prelude.< (a2 ! coord)) 
-
-filterWith :: (Unbox b, R.Source r a, R.Source s b, Monad m) =>
-    Array r DIM1 a -> (a -> Bool) -> Array s DIM1 b -> m (Array U DIM1 b)
-filterWith filterer cond filtered = R.selectP (cond . from filterer) (from filtered) length
-    where (Z :. length) = R.extent filtered
-          from :: (R.Source r c) => Array r DIM1 c -> Int -> c
-          from array n = array ! (Z :. n)
-
--- filterBy :: (R.Source r b, R.Source r a, Elt a, Unbox a, Monad m) =>
---   Array r DIM2 a -> (Array r DIM1 b, b -> Bool) -> m (Array U DIM1 a)
--- filtered `filterBy` (filterer, cond) = R.selectP (cond . distance') eltForNew (numRays * width)
---     where (Z :. _ :. width) = R.extent filtered
---           distance' n       = filterer ! (Z :. n `quot` width)
---           eltForNew n       = filtered ! (Z :. n `quot` width :. n `mod` width)
-
--- -- reshape after filtering
--- filterBy3 :: (Elt a, Unbox a) =>
---   Vec1 -> Array D DIM2 a -> (Double -> Bool) -> Maybe (Array D DIM2 a)
--- filterBy3 filterer filtered cond = do newArray <- filtered `filterBy` (filterer, cond)
---                                       let (Z :. length) = R.extent newArray
---                                       Just $ R.reshape (Z :. length `quot` 3 :. 3) newArray
-
--- expand dim0 before filtering
--- filterByDistance1 :: (Elt a, Unbox a) => (Double -> Bool) -> Vec1 -> Maybe (Array D DIM1 a)
--- filterBy1 :: (R.Source r b, R.Source r a, Elt a, Unbox a, Monad m) =>
---   Array D DIM1 a -> Array D DIM1 b -> (b -> Bool) -> Maybe (Array D DIM1 a)
--- filterBy1 :: (Elt a, Unbox a) =>
---     Vec1 -> Array D DIM1 a -> (Double -> Bool) -> Maybe (Array D DIM1 a)
--- filterBy1 filterer filtered cond = expanded `filterBy` (filterer, cond) >>= Just . R.delay
---         where expanded = R.reshape (Z :. length :. 1) filtered
---               (Z :. length) = R.extent filtered
-
-
-rayShape3 = (Z :. imgHeight * imgWidth :. 3)
-rayShape1 = (Z :. imgHeight * imgWidth)
-canvas = R.fromFunction rayShape3 $ const 0 :: Vec3
-
-rowAt :: Int -> Vec3 -> Vec3
-rowAt i = R.extract (Z :. i :. 0) (Z :. 1 :. 3)
-
-
-
--- applyColorTo :: Rays -> Vec3 -> Array D DIM2 Double
--- applyColorTo rays canvas = R.fromFunction rayShape3 $ \(Z :. i :. j) ->
---   let [rayColor, canvasColor] = map (rowAt i) [_colors rays, canvas]
---   in (rayColor *^ canvasColor) ! (Z :. 0 :. j)
-
-emptyRays = Rays { _origins   = empty3
-                 , _vectors   = empty3
-                 , _pixels    = R.computeS empty1
-                 , _distances = R.fromListUnboxed  (Z :. 0) []
-                 , _num       = 0 }
-  where empty1 = R.fromFunction (Z :. 0) $ const 0
-        empty3 = R.fromFunction (Z :. 0 :. 0) $ const 0
-
-vec3ToImage :: Vec3 -> ImageArray
-vec3ToImage array = R.reshape (Z :. imgHeight :. imgWidth)
-  $ R.fromFunction (Z :. imgHeight * imgWidth) rowToRGB
-  where rowToRGB (Z :. i) = let [r, g, b] = map round . R.toList $ rowAt i array
-                            in  newRGB r g b
-
+inf :: Double
+inf = 1 / 0
 
 inferMissing :: (Show a, Integral a) => [a] -> [a] -> [a]
 inferMissing list listWithNeg
@@ -178,6 +27,11 @@ inferMissing list listWithNeg
     where valid      = (product result == product list) && (all (> 0) result)
           missingVal = product list `quot` product (filter (>= 0) listWithNeg)
           result     = map (\x -> if x < 0 then missingVal else x) listWithNeg
+
+mapIndex ::
+  (S.Shape sh', R.Source r a) =>
+  (sh' -> b) -> Array r sh' a -> Array D sh' b
+mapIndex f array = R.traverse array id $ const f
 
 
 reshape :: (R.Source r1 e, S.Shape sh1, S.Shape sh2) => [Int] -> Array r1 sh1 e -> Array D sh2 e
@@ -191,7 +45,5 @@ expandDim :: (R.Source r1 e, S.Shape sh1, S.Shape sh2) => Int -> Array r1 sh1 e 
 expandDim dim array = R.reshape shape array
   where shape            = S.shapeOfList . (insertAt dim 1) . S.listOfShape $ R.extent array
 
-
 insertAt :: Int -> a -> [a] -> [a]
 insertAt n x list = (take n list) ++ [x] ++ (drop n list)
-  
