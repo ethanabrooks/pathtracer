39e07dc49830a45c7dd33244a78a5853d93c6f28
Ethan Brooks <ethanabrooks@gmail.com>
2017-09-05 16:03:34 -0400

    init

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,6 +12,7 @@ module Lib
   ) where
 
 import qualified Codec.Picture as P
+import Control.Arrow
 import Control.Monad
 import Data.Angle (Degrees(..), arccosine)
 import Data.Array.Repa ((:.)(..), Array, D, DIM3, DIM2, U, Z(..))
@@ -20,8 +21,8 @@ import Data.Conduit (($$), (=$=), Source, Producer, Conduit)
 import qualified Data.Conduit.List
 import qualified Data.Vector as V
 import Object
-       (Object(..), Ray(..), Point(..), Vector(..), getColor, getNormal,
-        distanceFrom, objects, march, getVector)
+       (Object(..), Ray(..), Point(..), Vector(..), Color(..), getColor,
+        getNormal, distanceFrom, objects, march, getVector)
 import qualified Params
 import qualified System.Random as Random
 import Triple (Triple(..), Vec3, normalize, dot)
@@ -61,6 +62,11 @@ traceSource =
     startValues = R.zipWith (,) blackCanvas startingGens
     computeArray3 = R.computeP . fromTripleArray . (R.map fst)
 
+fireRay :: Random.StdGen -> Int -> Int -> (Random.StdGen, Color)
+fireRay gen i j = first _gen $ iterate bounceRay' startValues !! Params.numIters
+  where
+    startValues = (rayFromCamToPixel gen (Z :. i :. j), Color white)
+
 traces
   :: Monad m
   => [m (Array U DIM3 Double)]
@@ -74,22 +80,40 @@ traceCanvas array =
   R.traverse array id $ \lookup sh ->
     let (color, gen) = lookup sh
         initialRay = rayFromCamToPixel gen sh
-        (newColor, gen') = terminalColor Params.maxBounces white initialRay
+        (newColor, gen') = bounceRay Params.maxBounces white initialRay
     in (color + newColor, gen')
 
-terminalColor :: Int -> Vec3 -> Ray -> (Vec3, Random.StdGen)
-terminalColor 0 _ ray = (black, _gen ray) -- ran out of bounces
-terminalColor bouncesLeft pixel ray = interactWith $ closestObjectTo ray
+bounceRay :: Int -> Vec3 -> Ray -> (Vec3, Random.StdGen)
+bounceRay 0 _ ray = (black, _gen ray) -- ran out of bounces
+bounceRay bouncesLeft pixel ray = interactWith $ closestObjectTo ray
   where
+    gen = _gen ray
     interactWith :: Maybe (Object, Double) -> (Vec3, Random.StdGen)
-    interactWith Nothing = (black, _gen ray) -- pixel
+    interactWith Nothing = (black, gen) -- pixel
     interactWith (Just (object, distance))
-      | hitLight = ((_emittance object *) <$> pixel, _gen ray)
-      | otherwise = terminalColor (bouncesLeft - 1) pixel' ray'
+      | hitLight = ((_emittance object *) <$> pixel, gen)
+      | otherwise = bounceRay (bouncesLeft - 1) pixel' ray'
       where
         hitLight = _emittance object > 0 :: Bool
-        ray' = bounceRay ray object distance :: Ray
+        origin' = march ray distance
+        (vector, gen') = reflectVector gen object $ getVector ray
+        ray' = Ray (Point origin') (Vector vector) gen' (Just object)
         pixel' = pixel * getColor object :: Vec3
+
+bounceRay' :: (Ray, Color) -> (Ray, Color)
+bounceRay' (ray, Color color) =
+  second Color . interactWith $ closestObjectTo ray
+  where
+    interactWith :: Maybe (Object, Double) -> (Ray, Vec3)
+    interactWith Nothing = (ray, black) -- pixel
+    interactWith (Just (object, distance))
+      | hitLight = (ray, (_emittance object *) <$> color)
+      | otherwise = (ray', color * getColor object)
+      where
+        hitLight = _emittance object > 0 :: Bool
+        origin' = march ray distance
+        (vector, gen') = reflectVector (_gen ray) object $ getVector ray
+        ray' = Ray (Point origin') (Vector vector) gen' (Just object)
 
 closestObjectTo :: Ray -> Maybe (Object, Double)
 closestObjectTo ray = do
@@ -111,13 +135,6 @@ closestObjectTo ray = do
     distanceOrdering (_, distance1) (_, distance2) = compare distance1 distance2
 
 ---
-bounceRay :: Ray -> Object -> Double -> Ray
-bounceRay ray@(Ray {_gen = gen}) object distance =
-  Ray (Point origin) (Vector vector) gen' (Just object)
-  where
-    origin = march ray distance
-    (vector, gen') = reflectVector gen object $ getVector ray
-
 reflectVector :: Random.StdGen -> Object -> Vec3 -> (Vec3, Random.StdGen)
 reflectVector gen object vector
   | _reflective object = specular gen 40 vector normal
